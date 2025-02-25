6ca2af56f95ace99883201f25e62569d6ce79301
Deepanshu Chauhan <deepanshuchauhan483@gmail.com>
2025-02-25 22:23:10 +0530

    adding live link for backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://pdf-app-eor0.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
